1ffd73fd1179478002e63bf049b28770348f3b1a
lunahilly <35861@ma-web.nl>
2026-04-15 12:13:39 +0200

    activities added 2 heads

--- opendag/resources/js/Pages/Activities/Activity.jsx
+++ opendag/resources/js/Pages/Activities/Activity.jsx
@@ -4,7 +4,6 @@ function Activity({data}){
             <h2>{data.course !== null ? `${data.course.name} : ` : null} {data.activity_type.name}</h2>
         </article>
     )
-
 }
 
 export default Activity;
