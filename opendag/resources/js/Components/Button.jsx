6b734b5ef1a734b8144f1c9b8696209b0bc02526
KenKoorn <35050@ma-web.nl>
2026-05-18 10:21:33 +0200

    Homepagina

--- opendag/resources/js/Components/Button.jsx
+++ opendag/resources/js/Components/Button.jsx
@@ -1,7 +1,8 @@
-const Button = ({label, onClick, route, type, isDisabled}) => {
+const Button = ({label, onClick, route, type, isDisabled, arrow}) => {
     return (
         <button onClick={onClick} type={type} disabled={isDisabled} className="button">
             {label}
+            {arrow && <span className="button__arrow">→</span>}
         </button>
     );
 }
