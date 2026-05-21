3010238eb4c773c82d4f1ec70ca3ba6583394cf5
smohammed576 <smohammed0576@gmail.com>
2026-05-21 17:34:49 +0200

    activity data.image change

--- opendag/resources/js/Pages/Activities/Activity.jsx
+++ opendag/resources/js/Pages/Activities/Activity.jsx
@@ -3,11 +3,7 @@ import image from "../../../../img/image.jpg"
 function Activity({ data, index }) {
     return (
         <article className="activity">
-<<<<<<< HEAD
             <img className="activity__img" src={data.image} alt="vul in" />
-=======
-            <img className="activity__img" src={image} alt="vul in" />
->>>>>>> 7aae4e705cde5a72c1a1538b453c9a3634c96e0a
             <div className="activity__text">
                 <h2 className="activity__title">{data.title}</h2>
                 <p className="activity__description">{data.description}</p>
